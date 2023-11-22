b1e96e64da766197af246bf33f0178636112d569
Karen Uzunyan <klaatuka82@gmail.com>
2023-11-22 18:04:57 +0300

    Initial Commit

---
+++ p5m2hm5 - UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  p5m2hm5 - UITableView
+//
+//  Created by Карен Узунян on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
